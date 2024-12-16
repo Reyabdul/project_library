7ff1dbfa20efb8b06ec1e4b4c074b79d3cc19855
Reyabdul <reyabdul@gmail.com>
2024-12-16 13:55:38 +0900

    Create form to add new books. Provide styling to display book info.

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-// alert("connected")
+//Library Array
+const myLibrary = ["Harry Potter", "The Lion King", "Lord of the Rings"];
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
+myLibrary.map(book => {
+  console.log(book);   
+});
+
+// function addBookTo Library(){
+
+// }
